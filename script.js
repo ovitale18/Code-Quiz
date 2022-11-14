4a572b6bf209f51c6dbf199623ab1cdd559091c5
Oliver Vitale <ovitale18@gmail.com>
2022-11-14 14:41:13 -0500

    i added a timer that counts down from 100 seconds

---
+++ script.js
@@ -0,0 +1,20 @@
+var timeEl = document.querySelector(".timer");
+var secondsLeft = 100;
+
+document.querySelector("#start").addEventListener("click", startTimer);
+
+
+function startTimer() {
+
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = " Time Remaining: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      
+      sendMessage();
+    }
+
+  }, 1000);
+}
